53d73f953af024f52751420f11ee039bb628fd6d
tanmayrane18 <tanmayrane51@gmail.com>
2025-01-05 22:24:35 +0530

    auth app completed

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { Colors } from './constants/styles';
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Colors } from "./constants/styles";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+import { useContext, useEffect, useState } from "react";
+import IconButton from "./components/ui/IconButton";
+
 
 const Stack = createNativeStackNavigator();
+
 
 function AuthStack() {
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
+        headerTintColor: "white",
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
@@ -25,33 +30,52 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const AuthCtx = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
+        headerTintColor: "white",
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon={"exit"}
+              color={tintColor}
+              size={24}
+              onPress={AuthCtx.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
 function Navigation() {
+  const AuthCtx = useContext(AuthContext);
   return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
+    <>
+      {AuthCtx.isAuthenticated && <AuthenticatedStack />}
+      {!AuthCtx.isAuthenticated && <AuthStack />}
+    </>
   );
 }
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <StatusBar style="light" />
+        <NavigationContainer>
+            <Navigation />
+        </NavigationContainer>
+      </AuthContextProvider>
     </>
   );
 }
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
+import axios from "axios";
 
 function WelcomeScreen() {
+  const AuthCtx = useContext(AuthContext);
+  const token = AuthCtx.token;
+  const [authenticatedMessage, setAuthenticatedMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://rn-auth-bdad6-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setAuthenticatedMessage(response.data)
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{authenticatedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +33,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
